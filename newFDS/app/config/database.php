0fa6841896ce2c0263dfd22ba21e039737e53b6a
rangsimant <hanwongsa.r@gmail.com>
2015-11-18 19:01:08 +0700

    change ip localhost to 127.0.0.1

--- newFDS/app/config/database.php
+++ newFDS/app/config/database.php
@@ -65,7 +65,7 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
+			'host'      => '127.0.0.1',
 			'database'  => 'stdbnew',
 			'username'  => 'root',
 			'password'  => '',
